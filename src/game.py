2b783882cc1cb775a5118252a7a02807c36d86b4
nyphoon <nyphoon@gmail.com>
2020-10-14 18:27:32 +0800

    refactor game to functions for other module using

--- src/game.py
+++ src/game.py
@@ -17,46 +17,65 @@ class Ship:
     # assume every point is life value 1
     return sum(self.life.values()) < len(self.life)
     
-def mark_to_coordinate(mark):
+def _mark_to_coordinate(mark):
   # coordinate system from (0,0)
   return int(mark[0]) - 1, ord(mark[1]) - 65
   
-def points_in_area(left_top, bottom_right):
+def _points_in_area(left_top, bottom_right):
   x = bottom_right[0] - left_top[0]
   y = bottom_right[1] - left_top[1]
   for i in range(x+1):
     for j in range(y+1):
       yield (left_top[0] + i, left_top[1] + j)
 
-def one_round(N, S, T):
-    ship_marks = []
-    for mark in S.split(','):
-      if mark:
-        ship_marks.append(mark)
-    ship_areas = []
-    for mark in ship_marks:
-      ship_areas.append([mark_to_coordinate(m) for m in mark.split()])
+def create_ships(S):
+  ship_marks = []
+  for mark in S.split(','):
+    if mark:
+      ship_marks.append(mark)
+  ship_areas = []
+  for mark in ship_marks:
+    ship_areas.append([_mark_to_coordinate(m) for m in mark.split()])
 
-    ships = []
-    for ship_area in ship_areas:
-      ship_body = points_in_area(ship_area[0], ship_area[1])
-      ship = Ship(*ship_body)
-      ships.append(ship)
-    
-    for attack in T.split():
-      attack_point = mark_to_coordinate(attack)
+  ships = []
+  for ship_area in ship_areas:
+    ship_body = _points_in_area(ship_area[0], ship_area[1])
+    ship = Ship(*ship_body)
+    ships.append(ship)
+  return ships
+
+def create_attacks(T):
+  attacks = []
+  for attack in T.split():
+      attacks.append(_mark_to_coordinate(attack))
+  return attacks
+
+def eval_game(ships, attacks):
+  for a in attacks:
       for ship in ships:
-        if ship.recieve_attack(attack_point):
+        if ship.recieve_attack(a):
           break
+  
+  sunk = damaged = undamaged = 0
+  for ship in ships:
+    if ship.is_alive():
+      if ship.is_attacked():
+        damaged += 1
+      else:
+        undamaged += 1
+    else:
+      sunk += 1
+  return {'sunk': sunk, 'alive': damaged, 'intact': undamaged}
 
-    # evaluate result
+def go_round(N, S, T):
+    ships = create_ships(S)
+    attacks = create_attacks(T)
+
     sunk = 0
     for ship in ships:
       if not ship.is_alive():
         sunk += 1
-    attacked_but_alive = 0
-    for ship in ships:
-      if ship.is_alive() and ship.is_attacked():
-        attacked_but_alive += 1
 
-    return '{},{}'.format(sunk, attacked_but_alive)
+    result = eval_game(ships, attacks)
+
+    return '{},{}'.format(result['sunk'], result['alive'])
--- src/test_game.py
+++ src/test_game.py
@@ -1,28 +1,28 @@
 import pytest
-from game import one_round
+from game import go_round
 
-def test_one_round():
+def test_go_round():
     N = 4
     S = '1B 2C,2D 4D'
     T = '2B 2D 3D 4D 4A'
-    assert '1,1' == one_round(N, S, T)
+    assert '1,1' == go_round(N, S, T)
 
     N = 3
     S = '1A 1B,2C 2C'
     T = '1B'
-    assert '0,1' == one_round(N, S, T)
+    assert '0,1' == go_round(N, S, T)
 
     N=12
     S='1A 2A,12A 12A'
     T='12A'
-    assert '1,0' == one_round(N, S, T)
+    assert '1,0' == go_round(N, S, T)
 
     N = 2
     S = '1A 2A'
     T = ''
-    assert '0,0' == one_round(N, S, T)
+    assert '0,0' == go_round(N, S, T)
 
     N = 26
     S = '1B 2C,2D 4D,5A 5C,6B 6C'
     T = '2A 4B'
-    assert '0,0' == one_round(N, S, T)
+    assert '0,0' == go_round(N, S, T)
